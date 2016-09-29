29cc3746b5f800eaabb88fb6e6f0dbf7a12ffa58
William Shepherd <williamrshepherd@gmail.com>
2016-09-28 23:19:26 -0500

    reformatted

--- Yelp Challenge Data Loader/Program.cs
+++ Yelp Challenge Data Loader/Program.cs
@@ -8,90 +8,90 @@ using System.Linq;
 namespace ConsoleApplication
 {
     public class Program
-	{
-		private static string basePath => Path.Combine(Directory.GetCurrentDirectory(), "data");
-		private static MySqlConnection connection => new MySqlConnection("server=localhost;user=root;password=P@ssword!;database=yelp;port=3306;");
+    {
+        private static string basePath => Path.Combine(Directory.GetCurrentDirectory(), "data");
+        private static MySqlConnection connection => new MySqlConnection("server=localhost;user=root;password=P@ssword!;database=yelp;port=3306;");
 
-		public static void Main(string[] args)
-		{
-			LoadReviews(connection);
+        public static void Main(string[] args)
+        {
+            LoadReviews(connection);
 
-			Console.WriteLine("Completed loading...");
-		}
+            Console.WriteLine("Completed loading...");
+        }
 
-		private static string GetFullFilename(string x) => Path.Combine(basePath, x + ".json");
+        private static string GetFullFilename(string x) => Path.Combine(basePath, x + ".json");
 
-		private static void LoadReviews(MySqlConnection connection)
-		{
-			var sqlFormat = 
-				@"INSERT INTO review (
-					business_id,
-					user_id,
-					stars,
-					text,
-					date,
-					votes_funny,
-					votes_useful,
-					votes_cool)
-				VALUES (
-					@business_id,
-					@user_id,
-					@stars,
-					@text,
-					@date,
-					@votes_funny,
-					@votes_useful,
-					@votes_cool);";
-			try
-			{
-				connection.Open();
+        private static void LoadReviews(MySqlConnection connection)
+        {
+            var sqlFormat = 
+                @"INSERT INTO review (
+                    business_id,
+                    user_id,
+                    stars,
+                    text,
+                    date,
+                    votes_funny,
+                    votes_useful,
+                    votes_cool)
+                VALUES (
+                    @business_id,
+                    @user_id,
+                    @stars,
+                    @text,
+                    @date,
+                    @votes_funny,
+                    @votes_useful,
+                    @votes_cool);";
+            try
+            {
+                connection.Open();
 
-				var jsonObjs = File
-					.ReadLines(GetFullFilename("review"))
-					.Select(x => JsonConvert.DeserializeObject(x))
-					.Select(x => {
-						dynamic obj = x;
+                var jsonObjs = File
+                    .ReadLines(GetFullFilename("review"))
+                    .Select(x => JsonConvert.DeserializeObject(x))
+                    .Select(x => {
+                        dynamic obj = x;
 
-						return new {
-							obj.business_id,
-							obj.user_id,
-							obj.stars,
-							obj.text,
-							obj.date,
-							votes_funny = obj.vots?.funny,
-							votes_useful = obj.votes?.useful,
-							votes_cool = obj.votes?.cool
-						};
-					});
+                        return new {
+                            obj.business_id,
+                            obj.user_id,
+                            obj.stars,
+                            obj.text,
+                            obj.date,
+                            votes_funny = obj.votes?.funny,
+                            votes_useful = obj.votes?.useful,
+                            votes_cool = obj.votes?.cool
+                        };
+                    });
 
-				foreach (var obj in jsonObjs) 
-				{
-					connection.Execute(sqlFormat, obj);
-				}			
-			}
-			finally
-			{
-				connection.Close();
-				connection.Dispose();
-			}	
-		}
+                foreach (var obj in jsonObjs) 
+                {
+                    connection.Execute(sqlFormat, obj);
+                }			
+            }
+            finally
+            {
+                connection.Close();
+                connection.Dispose();
+            }	
+        }
 
-		private static void LoadTips()
-		{
-			foreach (var line in File.ReadLines(GetFullFilename("business")))
-			{
+        private static void LoadTips()
+        {
+            foreach (var line in File.ReadLines(GetFullFilename("business")))
+            {
 
-			}
-		}
+            }
+        }
 
-		private static void LoadBusinesses()
-		{
+        private static void LoadBusinesses()
+        {
 
-		}
+        }
 
-		private static void ImportUsers()
-		{
+        private static void ImportUsers()
+        {
 
-		}
-	}
+        }
+    }
 }

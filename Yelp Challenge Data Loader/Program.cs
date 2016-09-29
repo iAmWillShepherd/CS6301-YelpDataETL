47ffbeecdc37ec6cc5c995f9062a023d7169c69f
William Shepherd <williamrshepherd@gmail.com>
2016-09-29 03:11:54 -0500

    Started implementing user import method

--- Yelp Challenge Data Loader/Program.cs
+++ Yelp Challenge Data Loader/Program.cs
@@ -15,7 +15,7 @@ namespace ConsoleApplication
 
         public static void Main(string[] args)
         {
-            LoadCheckins(connection);
+            LoadUsers(connection);
 
             Console.WriteLine("Completed loading...");
         }
@@ -124,9 +124,197 @@ namespace ConsoleApplication
 
         }
 
-        private static void LoadUsers()
+        private static void LoadUsers(MySqlConnection connection)
         {
+            var userSql = 
+                @"INSERT INTO user (
+                    user_id,
+                    name,
+                    review_count,
+                    average_stars,
+                    yelping_since,
+                    fans)
+                VALUES (
+                    @user_id,
+                    @name,
+                    @review_count,
+                    @average_stars,
+                    @yelping_since,
+                    @fans);";
 
+                var eliteStatusSql = 
+                    @"INSERT INTO user_elite_status (
+                        user_id,
+                        year)
+                    VALUES (
+                        @user_id,
+                        @year);";
+
+                var votesSql = 
+                    @"INSERT INTO user_vote (
+                        user_id,
+                        funny,
+                        useful,
+                        cool)
+                    VALUES (
+                        @user_id,
+                        @funny,
+                        @useful,
+                        @cool);";
+
+                var userFriendsSql = 
+                    @"INSERT INTO user_friend (
+                        user_id,
+                        friend_user_id)
+                    VALUES (
+                        @user_id,
+                        @friend_user_id);";
+
+                var userComplimentsSql = 
+                    @"INSERT INTO user_compliment (
+                        user_id,
+                        profile,
+                        cute,
+                        funny,
+                        plain,
+                        writer,
+                        list,
+                        note,
+                        photos,
+                        hot,
+                        cool,
+                        more)
+                    VALUES (
+                        @user_id,
+                        @profile,
+                        @cute,
+                        @funny,
+                        @plain,
+                        @writer,
+                        @list,
+                        @note,
+                        @photos,
+                        @hot,
+                        @cool,
+                        @more);";
+
+                var objs = File.ReadLines(GetFullFilename("user"))
+                    .Select(x => JsonConvert.DeserializeObject(x))
+                    .Select(x => {
+                        dynamic obj = x;
+
+                        var user = new {
+                            obj.user_id,
+                            obj.name,
+                            obj.review_count,
+                            obj.average_stars,
+                            yelping_since = ((string)obj.yelping_since) + "-01",
+                            obj.fans
+                        };
+
+                        var elite = new List<int>();
+                        var friends = new List<string>();
+                        var compliments = new Dictionary<string, int>();
+                        var votes = new Dictionary<string, int>();
+                        
+                        foreach (var year in obj.elite)
+                        {
+                            elite.Add((int)year);
+                        }
+
+                        foreach (var friend in obj.friends)
+                        {
+                            friends.Add((string)friend.Value);
+                        }
+
+                        foreach (var kvp in obj.compliments)
+                        {
+                            compliments.Add(kvp.Name, (int)kvp.Value.Value);
+                        }
+
+                        foreach (var kvp in obj.votes)
+                        {
+                            votes.Add(kvp.Name, (int)kvp.Value.Value);
+                        }
+
+                        return new {
+                            user,
+                            yearsElite = elite,
+                            friends,
+                            compliments,
+                            votes
+                        };
+                    });
+
+                connection.Open();
+                var transaction = connection.BeginTransaction();
+                try
+                {
+                    foreach (var obj in objs)
+                    {
+                        connection.Execute(userSql, obj.user);
+
+                        connection.Execute(
+                            userComplimentsSql,
+                            new {
+                                obj.user.user_id,
+                                profile = obj.compliments["profile"],
+                                cute = obj.compliments["cute"],
+                                funny = obj.compliments["funny"],
+                                plain = obj.compliments["plain"],
+                                writer = obj.compliments["writer"],
+                                list = obj.compliments["list"],
+                                note = obj.compliments["note"],
+                                photos = obj.compliments["photos"],
+                                hot = obj.compliments["hot"],
+                                cool = obj.compliments["cool"],
+                                more = obj.compliments["more"]
+                            });
+
+                        connection.Execute(
+                            votesSql,
+                            new {
+                                obj.user.user_id,
+                                funny = obj.votes["funny"],
+                                useful = obj.votes["useful"],
+                                cool = obj.votes["cool"]
+                            });
+
+                        foreach (var year in obj.yearsElite)
+                        {
+                            connection.Execute(
+                                eliteStatusSql, 
+                                new {
+                                    obj.user.user_id,
+                                    year
+                                });
+                        }
+
+                        foreach (var friend in obj.friends)
+                        {
+                            connection.Execute(
+                                userFriendsSql,
+                                new {
+                                    user_id = obj.user.user_id,
+                                    friend_user_id = friend
+                                });
+                        }
+                    }
+
+                    transaction.Commit();
+                }
+                catch
+                {
+                    transaction.Rollback();
+
+                    throw;
+                }
+                finally
+                {
+                    transaction.Dispose();
+                    connection.Close();
+                    connection.Dispose();
+                }
         }
 
         private static void LoadCheckins(MySqlConnection connection)
@@ -218,51 +406,50 @@ namespace ConsoleApplication
                     return record;
                 });
 
-                connection.Open();
+            connection.Open();
 
-                try
+            try
+            {
+                foreach (var obj in objs)
                 {
-                    foreach (var obj in objs)
+                    foreach (var kvp in obj.checkinInfo)
                     {
-                        foreach (var kvp in obj.checkinInfo)
-                        {
-                            connection.Execute(sql, new {
-                                obj.business_id,
-                                day_of_week_id = kvp.Key,
-                                hour_0 = kvp.Value[0],
-                                hour_1 = kvp.Value[1],
-                                hour_2 = kvp.Value[2],
-                                hour_3 = kvp.Value[3],
-                                hour_4 = kvp.Value[4],
-                                hour_5 = kvp.Value[5],
-                                hour_6 = kvp.Value[6],
-                                hour_7 = kvp.Value[7],
-                                hour_8 = kvp.Value[8],
-                                hour_9 = kvp.Value[9],
-                                hour_10 = kvp.Value[10],
-                                hour_11 = kvp.Value[11],
-                                hour_12 = kvp.Value[12],
-                                hour_13 = kvp.Value[13],
-                                hour_14 = kvp.Value[14],
-                                hour_15 = kvp.Value[15],
-                                hour_16= kvp.Value[16],
-                                hour_17 = kvp.Value[17],
-                                hour_18 = kvp.Value[18],
-                                hour_19 = kvp.Value[19],
-                                hour_20 = kvp.Value[20],
-                                hour_21 = kvp.Value[21],
-                                hour_22 = kvp.Value[22],
-                                hour_23 = kvp.Value[23]
-                            });
-                        }
+                        connection.Execute(sql, new {
+                            obj.business_id,
+                            day_of_week_id = kvp.Key,
+                            hour_0 = kvp.Value[0],
+                            hour_1 = kvp.Value[1],
+                            hour_2 = kvp.Value[2],
+                            hour_3 = kvp.Value[3],
+                            hour_4 = kvp.Value[4],
+                            hour_5 = kvp.Value[5],
+                            hour_6 = kvp.Value[6],
+                            hour_7 = kvp.Value[7],
+                            hour_8 = kvp.Value[8],
+                            hour_9 = kvp.Value[9],
+                            hour_10 = kvp.Value[10],
+                            hour_11 = kvp.Value[11],
+                            hour_12 = kvp.Value[12],
+                            hour_13 = kvp.Value[13],
+                            hour_14 = kvp.Value[14],
+                            hour_15 = kvp.Value[15],
+                            hour_16= kvp.Value[16],
+                            hour_17 = kvp.Value[17],
+                            hour_18 = kvp.Value[18],
+                            hour_19 = kvp.Value[19],
+                            hour_20 = kvp.Value[20],
+                            hour_21 = kvp.Value[21],
+                            hour_22 = kvp.Value[22],
+                            hour_23 = kvp.Value[23]
+                        });
                     }
                 }
-                finally
-                {
-                    connection.Clone();
-                    connection.Dispose();
-                }
-            
+            }
+            finally
+            {
+                connection.Clone();
+                connection.Dispose();
+            }
         }
     }
 }

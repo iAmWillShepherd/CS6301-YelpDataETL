5cd14535dad8e3f3dc5f4ce72ac544447cdd4d8f
William Shepherd <iamwill@Athena.local>
2016-09-28 19:02:09 -0500

    Added data loader project for yelp data

---
+++ Yelp Challenge Data Loader/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+using Dapper;
+using System.Linq;
+using System.IO;
+using Newtonsoft;
+
+namespace ConsoleApplication
+{
+    public class Program
+	{
+		private static string basePath => Path.Combine(Directory.GetCurrentDirectory(), "data");
+		private static string connectionString => "server=localhost;user=root;password=P@ssword!;database=yelp;port=3306;";
+
+		public static void Main(string[] args)
+		{
+			foreach (var file in Directory.GetFiles(basePath, "*.json"))
+			{
+				foreach (var line in File.ReadLines(file))
+				{
+					var json = Newtonsoft.Json.JsonConvert.DeserializeObject(line);
+				}
+			}
+		}
+
+		private static string GetFullFilename(string x) => Path.Combine(basePath, x + ".json");
+
+		private static void ImportBusinesses()
+		{
+			foreach (var line in File.ReadLines(GetFullFilename("business")))
+			{
+
+			}
+		}
+
+		private static void ImportTips()
+		{
+			foreach (var line in File.ReadLines(GetFullFilename("business")))
+			{
+
+			}
+		}
+
+		private static void ImportReviews()
+		{
+
+		}
+
+		private static void ImportUsers()
+		{
+
+		}
+	}
+}

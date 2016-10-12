8f20d61e661b88560676c80b5acd58678399a7d8
William Shepherd <williamrshepherd@gmail.com>
2016-10-12 01:19:04 -0500

    Refactored coded to make it easier to read

--- Yelp Challenge Data Loader/Helpers.cs
+++ Yelp Challenge Data Loader/Helpers.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using System.IO;
 
 namespace YelpDataLoader
@@ -6,5 +8,18 @@ namespace YelpDataLoader
     {
         public static string GetFullFilename(string x) => Path.Combine(basePath, x + ".json");
         private static string basePath => Path.Combine(Directory.GetCurrentDirectory(), "data");
+
+        public static IEnumerable<TSource> DistinctBy<TSource, TKey>
+            (this IEnumerable<TSource> source, Func<TSource, TKey> keySelector)
+        {
+            HashSet<TKey> seenKeys = new HashSet<TKey>();
+            foreach (TSource element in source)
+            {
+                if (seenKeys.Add(keySelector(element)))
+                {
+                    yield return element;
+                }
+            }
+        }
     }
 }
